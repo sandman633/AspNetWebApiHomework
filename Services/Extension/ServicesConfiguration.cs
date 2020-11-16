80e8e1db58b6124c6dfc542e21fd327d91dbaf3d
sandman633 <69395587+sandman633@users.noreply.github.com>
2020-11-16 22:45:43 +0300

    task1 done

---
+++ Services/Extension/ServicesConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Services.Interfaces;
+using Services.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Services.Extension
+{
+    public static class ServicesConfiguration
+    {
+        public static void ConfigureServices(this IServiceCollection services)
+        {
+            services.AddTransient<ICarService, CarService>();
+        }
+    }
+}
---
+++ Services/Services/CarService.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using DataBase.Mocks;
+using Models.DTO;
+using Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Services.Services
+{
+    class CarService : ICarService
+    {
+        private readonly IMapper _mapper;
+
+        public CarService(IMapper mapper)
+        {
+            _mapper = mapper;
+        }
+
+
+        public IEnumerable<CarDto> GetCars()
+        {
+            var cars = CarMock.GetCars();
+            var response = _mapper.Map<IEnumerable<CarDto>>(cars);
+            return response;
+        }
+    }
+}
